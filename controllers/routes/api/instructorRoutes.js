b36e0753487accc02538cb91ad0db709a56c3866
Thara Supasiti <sapiensis@gmail.com>
2021-08-30 12:51:14 +1000

    add create instructor sign up api

---
+++ controllers/routes/api/instructorRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const services = require("../../services");
+const sanitize = require("../../services/sanitize");
+
+// Route : api/instructors/
+
+// get all students
+const getAllInstructors = async (req, res) => {
+  try {
+    const rawUsers = await services.instructor.getAll();
+    const cleanedUsers = sanitize(rawUsers);
+    res.status(200).json(cleanedUsers);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// create student account
+const createAccount = async (req, res) => {
+  try {
+    const rawInstructor = await services.instructor.create(req.body);
+    const cleanedInstructor = sanitize(rawInstructor);
+    res.status(200).json(cleanedInstructor);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+// router
+
+router.get("/", getAllInstructors);
+router.post("/signup", createAccount);
+
+module.exports = router;
--- controllers/services/index.js
+++ controllers/services/index.js
@@ -1,7 +1,9 @@
 const user = require("./userServices");
 const student = require("./studentServices");
+const instructor = require("./instructorServices");
 
 module.exports = {
   user,
   student,
+  instructor,
 };
