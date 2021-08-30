32f6f2d174ab046ae7fac17f2eabef4c27a4502e
Thara Supasiti <sapiensis@gmail.com>
2021-08-30 12:38:07 +1000

    add encryption to password

--- controllers/services/studentServices.js
+++ controllers/services/studentServices.js
@@ -1,4 +1,6 @@
 const models = require("../../models");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // get all students
 // return
@@ -15,11 +17,30 @@ const getAll = async () => {
   return result;
 };
 
+const getStudentData = async (newStudent) => {
+  const { student_name, email, password } = newStudent;
+  const encryptedPassword = await bcrypt.hash(password, saltRounds);
+  return {
+    student_name,
+    user: {
+      email,
+      password: encryptedPassword,
+    },
+  };
+};
+
 // create new student
+// argument : {
+//   student_name
+//   email
+//   password
+// }
 // return
 //  - Student
 const create = async (newStudent) => {
-  const result = await models.Student.create(newStudent, {
+  const studentData = await getStudentData(newStudent);
+  console.log(studentData);
+  const result = await models.Student.create(studentData, {
     include: [models.User],
   });
   return result;
