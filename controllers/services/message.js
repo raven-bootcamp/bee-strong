f9038fc433d682ccdbd0d53084a4cb3ff6792c0e
Thara Supasiti <sapiensis@gmail.com>
2021-09-04 15:33:04 +1000

    add farewell email message

--- controllers/services/message.js
+++ controllers/services/message.js
@@ -24,26 +24,29 @@ const getWelcome = (email) => {
 
 //-------------------------------------
 const farewellText = `
-Welcome to the hive!
+We are sorry to hear that you are leaving our hive :(
     
-We hope that you are buzzing with excitement to get beeeesy training
+We hope you had a good time with us! 
+
+May the honey flows and flowers forever bloom....
 
 Your Queen,
 `;
 
 const farewellHtml = `
-<p><b>Welcome to the hive!</b></p>
-<p>We hope that you are buzzing with excitement to get beeeesy training</p>
+<p><b>We are sorry to hear that you are leaving our hive :(</b></p>
+<p>We hope you had a good time with us!</p>
+<p>May the honey flows and flowers forever bloom....</p>
 <p>Your Queen,<br/>
   <img src="https://crazyleafdesign.com/blog/wp-content/uploads/2016/10/bee-strong.jpg"/></p>
 `;
 
 const getFarewell = (email) => {
   return {
-    to: `${email}`, // list of receivers
-    subject: "Welcome to the hive!", // Subject line
-    text: welcomeText, // plain text body
-    html: welcomeHtml, // html of message
+    to: `${email}`,
+    subject: "Sorry to hear you want to leave our hive!",
+    text: farewellText,
+    html: farewellHtml,
   };
 };
 
